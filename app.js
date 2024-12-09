8561f3d6c617ae0517fd18e369705fa6570dba6f
weirdinside <ani@weirdinsi.de>
2024-12-08 23:27:51 -0500

    bug fixes with CORS

--- app.js
+++ app.js
@@ -1,36 +1,66 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const { errors } = require("celebrate");
 const mongoose = require("mongoose");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const router = require("./routes/index");
-const { errorHandler } = require('./middlewares/error-handler');
+const { errorHandler } = require("./middlewares/error-handler");
 
-const helmet = require('helmet');
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
 
 const app = express();
 const port = 3001;
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.on('error', (error)=> console.error(error));
-db.on('open', ()=> console.log('connected to database'))
+db.on("error", (error) => console.error(error));
+db.on("open", () => console.log("connected to database"));
 
-app.use(express.urlencoded({extended: true}));
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 1000,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+});
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(requestLogger);
 
-app.use(helmet());
-app.use(cors());
-app.use('/', router);
+app.use(limiter);
 
-app.use('/thumbnails', express.static('thumbnails'));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+app.use("/thumbnails", cors(), express.static("thumbnails", {
+  setHeaders: (res, path) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Cross-Origin-Resource-Policy', 'cross-origin');
+  }
+}));
+
+app.use("/", router);
 
 app.use(errors());
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Listening on port http://localhost:${port}`)
-})
+  console.log(`Listening on port http://localhost:${port}`);
+});
